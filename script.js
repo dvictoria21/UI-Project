90ef61494f1c797e7661e672dec7ae54ec76e2db
Daija Watt <daijawatt@Daijas-MacBook-Pro.local>
2023-07-17 15:04:11 -0400

    connecting Tasty API

---
+++ script.js
@@ -0,0 +1,9 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bd1fa12b49msh6da84797acf39b6p153d6djsn24ee2287ea04",
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+  },
+};
+
+let url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=breakfast";
